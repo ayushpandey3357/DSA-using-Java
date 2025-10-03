639edf0ddaa4a7e4aabe7276d998fd4a0e6e7c5c
Ayush Kumar Pandey <ayushpandey1974@gmail.com>
2025-10-03 18:10:30 +0530

    Sorting
    All about sorting which comes in interview , you can consider it as basic file of sorting because i cover only that topics which are majorly ask in interview .

---
+++ Sorting/Inbuilt_sort.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Inbuilt_sort {
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,2,3};
+        //Arrays.sort(arr);
+        //Arrays.sort(arr,3,arr.length-1);
+        //Arrays.sort(arr, Collections.reverseOrder());
+        //Arrays.sort(arr,0,arr.length-1,Collections.reverseOrder());
+        System.out.println(Arrays.toString(arr));
+    }
+}
